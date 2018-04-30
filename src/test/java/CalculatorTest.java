ae21c592010199a0f9795953a8d96cc71bee34f5
Liam Cavens <liamcavens@googlemail.com>
2018-04-30 17:05:20 +0100

    Working Calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private Calculator testCalculator;
+
+    @Before
+    public void before() {
+        testCalculator = new Calculator(50, 10);
+    }
+
+    @Test
+    public void hasNumber1() {
+        assertEquals(50, testCalculator.getNumber1());
+    }
+
+    @Test
+    public void addFunction() {
+        assertEquals(60, testCalculator.add());
+    }
+
+    @Test
+    public void subtractFunction() {
+        assertEquals(40, testCalculator.subtract());
+    }
+
+    @Test
+    public void multiplyFunction() {
+        assertEquals(500, testCalculator.multiply());
+    }
+
+    @Test
+    public void divideFunction() {
+        assertEquals(5, testCalculator.divide());
+    }
 }
